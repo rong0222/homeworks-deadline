b0f15a54f2351a954448efbdd061222eb2e768d1
Ming-der Wang <mingderwang@gmail.com>
2018-06-18 23:04:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# live demo
+[https://hello-apollo-react-next-ewapauuihh.now.sh](https://hello-apollo-react-next-ewapauuihh.now.sh)
+
+# build
+```
+yarn install
+yarn dev
+```
+
+# deploy
+```
+yarn global add now
+now --public
+```
---
+++ pages/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import fetch from 'node-fetch';
+
+import PostList from './PostList';
+
+class App extends Component {
+  constructor(...args) {
+    super(...args);
+
+  this.client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      fetch,
+      uri: 'https://1jzxrj179.lp.gql.zone/graphql',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+  }
+  render() {
+    return (
+      <ApolloProvider client={this.client}>
+        <PostList />
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
