4fed625f1ee4e742c0403cfecf1b994a20e70412
Ming-der Wang <mingderwang@gmail.com>
2018-06-20 18:46:08 +0800

    add AddNewExam page

---
+++ pages/AddNewExam.js
@@ -0,0 +1,91 @@
+import { Component } from 'react'
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+        this.handleDateChange = this.handleDateChange.bind(this);
+    }
+
+    static defaultProps = {
+        messages: []
+    }
+
+    // init state with the prefetched messages
+    state = {
+        field: '',
+        fieldDate: '',
+        messages: this.props.messages
+    }
+
+    // connect to WS server and listen event
+    componentDidMount() {
+    }
+
+    // close socket connection
+    componentWillUnmount() {
+    }
+
+    // add messages from server to the state
+    handleMessage = (message) => {
+        this.setState(state => ({ messages: state.messages.concat(message) }))
+    }
+
+    handleChange = event => {
+        this.setState({ field: event.target.value })
+    }
+
+    handleDateChange = event => {
+        this.setState({ fieldDate: event.target.value })
+    }
+
+    // send messages to server and add them to the state
+    handleSubmit = event => {
+        event.preventDefault()
+
+        // create message object
+        const message = {
+            id: (new Date()).getTime(),
+            value: this.state.field,
+            date: this.state.fieldDate
+        }
+
+        // add it to state and clean current input value
+        this.setState(state => ({
+            messages: state.messages.concat(message)
+        }))
+    }
+
+    render() {
+        return (
+            <main>
+                <div>
+                    <ul>
+                        {this.state.messages.map(message =>
+                            <li key={message.id}>{message.value} on {message.date}</li>
+                        )}
+                    </ul>
+                    <h1>新增考試或作業名稱與日期</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            onChange={this.handleChange}
+                            type='text'
+                            placeholder='Final Exam for Computer Class'
+                            value={this.state.field}
+                        />
+                        <p />
+                        <input
+                            onChange={this.handleDateChange}
+                            type='text'
+                            placeholder='2018-6-28'
+                            value={this.state.fieldDate}
+                        />
+                        <p />
+                        <button>新增</button>
+                    </form>
+                </div>
+            </main>
+        )
+    }
+}
+
+export default HomePage
